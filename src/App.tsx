dc59d42326edea5468a8196c9c6e82f7febb6a0a
tae woo <kwixs35@gmail.com>
2022-11-11 20:35:54 +0900

    update UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,9 @@
 import React, { useEffect, useState } from "react";
+import { Breadcrumb, Layout, Menu } from "antd";
+const { Header, Content, Footer } = Layout;
+import { domainToASCII } from "url";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import "./App.css";
 
 function App() {
   const [time, setTime] = useState(new Date());
@@ -12,9 +17,45 @@ function App() {
   }, [timer]);
 
   return (
-    // DOM
-    <div>
+    <Layout className="layout">
       <h3>현재 시간 : {time.toLocaleTimeString()}</h3>
+      <Router>
+        <Header>
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={["1"]}
+            items={[
+              {
+                key: 1,
+                label: <Link to="/">Home</Link>,
+              },
+              {
+                key: 2,
+                label: <Link to="/resume">이력서</Link>,
+              },
+              {
+                key: 3,
+                label: <Link to="/portfolio">포트폴리오</Link>,
+              },
+            ]}
+          />
+        </Header>
+        <Layout.Content style={{ padding: "100 50px" }}>
+          <Routes>
+            <Route path="/portfolio" element={<Portfolio />} />
+            <Route path="/resume" element={<Resume />} />
+            <Route path="/home" element={<Home />} />
+          </Routes>
+        </Layout.Content>
+      </Router>
+    </Layout>
+  );
+}
+
+const Resume = () => {
+  return (
+    <div>
       <h3>자기소개</h3>
       <h3>이름</h3>
 
@@ -26,6 +67,14 @@ function App() {
       <li>이제 쌓아갈 예정</li>
     </div>
   );
-}
+};
+
+const Portfolio = () => {
+  return <></>;
+};
+
+const Home = () => {
+  return <></>;
+};
 
 export default App;
